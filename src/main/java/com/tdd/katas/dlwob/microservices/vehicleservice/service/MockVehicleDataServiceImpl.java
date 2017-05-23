895c09833bdd65d752ab8821f508261604af9e58
serdar.suat <serdar.suat@hexad.es>
2017-05-23 16:39:16 +0200

    MockVehicleDataServiceImpl New Test: Return_Valid_Data_For_An_Existent_Vehicle Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockVehicleDataServiceImpl.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockVehicleDataServiceImpl.java
@@ -1,17 +1,58 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleData;
+import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleInformation;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Hexad GmbH on 23/05/2017.
  */
 public class MockVehicleDataServiceImpl implements VehicleDataService{
 
-
     @Override
     public VehicleData getVehicleData(String vin)  {
-            throw new UnsupportedOperationException("Will be implemented");
+
+        if(SAMPLE_VEHICLE_VIN_CODE.equals(vin))
+            return sampleVehicleData;
+        else
+            return null;
+
+
     }
 
+    public static final String SAMPLE_VEHICLE_VIN_CODE = "sample-vehicle-vin-code";
+
+    public MockVehicleDataServiceImpl() {
+        readJsonFile();
+    }
+
+    private VehicleData sampleVehicleData;
+
+    private void readJsonFile() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String resourceName = getClass().getSimpleName() + ".json";
+
+        InputStream resourceInputStream = null;
+        try {
+            resourceInputStream = getClass().getResourceAsStream(resourceName);
+
+            VehicleData viData = objectMapper.readValue(resourceInputStream, VehicleData.class);
+
+            sampleVehicleData = viData;
+        } catch (Exception e) {
+            throw new IllegalStateException("Unexpected error reading from JSON file", e);
+        } finally {
+            if (resourceInputStream!=null) {
+                try {
+                    resourceInputStream.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
 
 }

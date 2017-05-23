22b5f3c3a105d5f382584142b9515de48be2e93a
serdar.suat <serdar.suat@hexad.es>
2017-05-23 17:52:40 +0200

    MockCustomerDataServiceImpl New Test: Returns_Valid_Data_For_An_Existent_Customer_Id Failing test

--- src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockCustomerDataServiceImplTest.java
+++ src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockCustomerDataServiceImplTest.java
@@ -3,6 +3,8 @@ package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.CustomerData;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 /**
@@ -20,5 +22,16 @@ public class MockCustomerDataServiceImplTest {
         assertNull(actualCustomerData);
     }
 
+    @Test
+    public void Returns_Valid_Data_For_An_Existent_Customer_Id(){
+        MockCustomerDataServiceImpl service=new MockCustomerDataServiceImpl();
+
+        CustomerData actualCustomerData=service.getCustomerData("sample-customer-id");
+
+        assertNotNull(actualCustomerData);
+        assertEquals("sample-customer-id",actualCustomerData.getId());
+        assertEquals("Sergio",actualCustomerData.getName());
+        assertEquals("Osuna Medina",actualCustomerData.getName());
+    }
 
 }

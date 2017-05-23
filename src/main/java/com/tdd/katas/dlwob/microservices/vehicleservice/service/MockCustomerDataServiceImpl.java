513fb4c1a9fc1bcd35332bc2bb593d9658275bbb
serdar.suat <serdar.suat@hexad.es>
2017-05-23 17:42:37 +0200

    MockCustomerDataServiceImpl New Test: Returns_Null_For_Non_Existent_Customer_Id Failing Test

---
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockCustomerDataServiceImpl.java
@@ -0,0 +1,13 @@
+package com.tdd.katas.dlwob.microservices.vehicleservice.service;
+
+import com.tdd.katas.dlwob.microservices.vehicleservice.model.CustomerData;
+
+/**
+ * Created by Hexad GmbH on 23/05/2017.
+ */
+public class MockCustomerDataServiceImpl {
+    public CustomerData getCustomerData(String s) {
+
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockCustomerDataServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.tdd.katas.dlwob.microservices.vehicleservice.service;
+
+import com.tdd.katas.dlwob.microservices.vehicleservice.model.CustomerData;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by Hexad GmbH on 23/05/2017.
+ */
+public class MockCustomerDataServiceImplTest {
+
+
+    @Test
+    public void Returns_Null_For_Non_Existent_Customer_Id(){
+
+        MockCustomerDataServiceImpl service=new MockCustomerDataServiceImpl();
+
+        CustomerData actualCustomerData=service.getCustomerData("Non existent customer id");
+        assertNull(actualCustomerData);
+
+    }
+
+
+}

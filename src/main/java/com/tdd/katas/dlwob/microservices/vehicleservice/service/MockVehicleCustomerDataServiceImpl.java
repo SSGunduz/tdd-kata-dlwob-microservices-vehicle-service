9b8a1158081007c0056b9a878cafcc12e4d878f7
serdar.suat <serdar.suat@hexad.es>
2017-05-24 10:58:42 +0200

    MockVehicleCustomerDataServiceImpl New Test: Returns_Valid_Data_For_Existent_Vin Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockVehicleCustomerDataServiceImpl.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockVehicleCustomerDataServiceImpl.java
@@ -5,8 +5,16 @@ import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleCustomerDat
 /**
  * Created by Hexad GmbH on 24/05/2017.
  */
-public class MockVehicleCustomerDataServiceImpl {
-    public VehicleCustomerData getVehicleCustomerData(String s) {
-        return null;
+public class MockVehicleCustomerDataServiceImpl extends AbstractMockServiceImpl<VehicleCustomerData> {
+
+    public MockVehicleCustomerDataServiceImpl() {
+        super(VehicleCustomerData.class);
+    }
+
+    public VehicleCustomerData getVehicleCustomerData(String vinCode) {
+        if("sample-vehicle-vin-code".equals(vinCode))
+            return dtoObject;
+        else
+            return null;
     }
 }

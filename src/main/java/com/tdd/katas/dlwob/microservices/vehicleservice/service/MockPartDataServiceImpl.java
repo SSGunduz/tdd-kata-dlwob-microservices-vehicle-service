4e3dfcd8439516c9bb63649ab764f04d8adaccb6
serdar.suat <serdar.suat@hexad.es>
2017-05-24 14:13:30 +0200

    Implementation refactoring: extracted interface PartDataService

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockPartDataServiceImpl.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/MockPartDataServiceImpl.java
@@ -8,12 +8,13 @@ import java.util.List;
 /**
  * Created by Hexad GmbH on 24/05/2017.
  */
-public class MockPartDataServiceImpl extends AbstractMockServiceImpl<List<PartData>>{
+public class MockPartDataServiceImpl extends AbstractMockServiceImpl<List<PartData>> implements PartDataService {
 
     protected MockPartDataServiceImpl() {
         super(new TypeReference<List<PartData>>(){});
     }
 
+    @Override
     public List<PartData> getPartData(String vinCode) {
 
         if(MockServicesConstants.SAMPLE_VEHICLE_VIN_CODE.equals(vinCode))

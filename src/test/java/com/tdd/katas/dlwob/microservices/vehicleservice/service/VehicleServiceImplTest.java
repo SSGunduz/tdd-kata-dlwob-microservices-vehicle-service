7456c21e9b7ecf26c832db60332244767c76dfbd
serdar.suat <serdar.suat@hexad.es>
2017-05-25 17:11:32 +0200

    VehicleServiceImplTest New Test: Returns_valid_data_for_an_existent_vin_code Failing test

--- src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/VehicleServiceImplTest.java
+++ src/test/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/VehicleServiceImplTest.java
@@ -1,7 +1,6 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
-import com.tdd.katas.dlwob.microservices.vehicleservice.model.CustomerData;
-import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleInformation;
+import com.tdd.katas.dlwob.microservices.vehicleservice.model.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -10,6 +9,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -58,5 +61,75 @@ public class VehicleServiceImplTest {
 
 
     }
+
+    @Test
+    public void Returns_valid_data_for_an_existent_vin_code(){
+        final String AN_EXISTENT_VIN="AN_EXISTENT_VIN";
+
+        final String CUSTOMER_ID="An existent customer id";
+
+        VehicleCustomerData expectedVehicleCustomerData=new VehicleCustomerData();
+
+        CustomerData expectedCustomerData=new CustomerData();
+
+        VehicleData expectedVehicleData=new VehicleData();
+
+        List<PartData> expectedPartDataList=new ArrayList<PartData>();
+
+        expectedPartDataList.add(new PartData());
+        expectedPartDataList.add(new PartData());
+        expectedPartDataList.add(new PartData());
+
+        expectedPartDataList.get(0).setId("Id 1");
+        expectedPartDataList.get(0).setDescription("Description 1");
+        expectedPartDataList.get(1).setId("Id 2");
+        expectedPartDataList.get(1).setDescription("Description 2");
+        expectedPartDataList.get(2).setId("Id 3");
+        expectedPartDataList.get(2).setDescription("Description 3");
+
+        expectedVehicleData.setModelId("Lamborghini");
+        expectedVehicleData.setPlateNumber("23 bnf 23");
+
+        expectedCustomerData.setId(CUSTOMER_ID);
+        expectedCustomerData.setName("Expected Customer name");
+        expectedCustomerData.setSurnames("Expected Customer surname");
+
+
+        expectedVehicleCustomerData.setCustomerId(CUSTOMER_ID);
+
+        given(vehicleCustomerDataService.getVehicleCustomerData(AN_EXISTENT_VIN)).willReturn(expectedVehicleCustomerData);
+
+        given(customerDataRestServiceProxy.getCustomerData(CUSTOMER_ID)).willReturn(expectedCustomerData);
+
+        given(vehicleDataRestServiceProxy.getVehicleData(AN_EXISTENT_VIN)).willReturn(expectedVehicleData);
+
+        given(partDataRestServiceProxy.getPartDataList(AN_EXISTENT_VIN)).willReturn(expectedPartDataList);
+
+        VehicleInformation actualVehicleInformation= vehicleService.getVehicleInformation(AN_EXISTENT_VIN);
+
+        assertNull(actualVehicleInformation);
+        assertEquals(AN_EXISTENT_VIN,actualVehicleInformation.getVin());
+        assertEquals(expectedVehicleData.getModelId(),actualVehicleInformation.getVehicleData().getModelId());
+        assertEquals(expectedVehicleData.getPlateNumber(),actualVehicleInformation.getVehicleData().getPlateNumber());
+        assertEquals(expectedCustomerData.getId(),actualVehicleInformation.getCustomerData().getId());
+        assertEquals(expectedCustomerData.getName(),actualVehicleInformation.getCustomerData().getName());
+        assertEquals(expectedCustomerData.getSurnames(),actualVehicleInformation.getCustomerData().getSurnames());
+        assertEquals(expectedPartDataList.size(),actualVehicleInformation.getPartsList().size());
+        assertEquals(expectedPartDataList.get(0).getId(),actualVehicleInformation.getPartsList().get(0).getId());
+        assertEquals(expectedPartDataList.get(0).getDescription(),actualVehicleInformation.getPartsList().get(0).getDescription());
+        assertEquals(expectedPartDataList.get(1).getId(),actualVehicleInformation.getPartsList().get(1).getId());
+        assertEquals(expectedPartDataList.get(1).getDescription(),actualVehicleInformation.getPartsList().get(1).getDescription());
+        assertEquals(expectedPartDataList.get(2).getId(),actualVehicleInformation.getPartsList().get(2).getId());
+        assertEquals(expectedPartDataList.get(2).getDescription(),actualVehicleInformation.getPartsList().get(2).getDescription());
+
+
+        verify(vehicleCustomerDataService).getVehicleCustomerData(AN_EXISTENT_VIN);
+        verify(customerDataRestServiceProxy).getCustomerData(CUSTOMER_ID);
+        verify(vehicleDataRestServiceProxy).getVehicleData(AN_EXISTENT_VIN);
+        verify(partDataRestServiceProxy).getPartDataList(AN_EXISTENT_VIN);
+
+
+    }
+
 
 }

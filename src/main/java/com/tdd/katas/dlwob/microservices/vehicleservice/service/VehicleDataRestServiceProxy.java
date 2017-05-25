70b3eaa0ca1ff056dbf18c1577bcd29389e66b93
serdar.suat <serdar.suat@hexad.es>
2017-05-25 12:07:42 +0200

    VehicleDataRestServiceProxy New Test: Returns_null_when_server_returns_404 Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/VehicleDataRestServiceProxy.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/VehicleDataRestServiceProxy.java
@@ -1,8 +1,13 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.controller.VehicleDataController;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleData;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Hexad GmbH on 25/05/2017.
@@ -10,12 +15,29 @@ import org.springframework.stereotype.Component;
 @Component
 class VehicleDataRestServiceProxy {
 
+    private RestTemplate restTemplate;
+
     public VehicleDataRestServiceProxy(RestTemplateBuilder restTemplateBuilder){
-        restTemplateBuilder.build();
+        restTemplate = restTemplateBuilder.build();
     }
 
-    public VehicleData getVehicleData(String vinCode) {
-        return null;
+    public VehicleData getVehicleData(String vinCode) throws HttpClientErrorException {
+        ResponseEntity<VehicleData> response;
+
+        try{
+            response =restTemplate.getForEntity(VehicleDataController.URL_MAPPING+"/"+vinCode, VehicleData.class);
+
+
+        }catch (HttpClientErrorException e) {
+            if(e.getStatusCode().equals(HttpStatus.NOT_FOUND))
+                return null;
+
+            else
+                throw e;
+        }
+
+        throw new UnsupportedOperationException("Not implemented");
+
     }
 
 }

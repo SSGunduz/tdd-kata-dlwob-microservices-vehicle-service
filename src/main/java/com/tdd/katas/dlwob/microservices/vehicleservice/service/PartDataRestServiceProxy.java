9bdd2fc8c0e855520c2a6dcce2017f681fa60f67
serdar.suat <serdar.suat@hexad.es>
2017-05-25 15:14:58 +0200

    PartDataRestServiceProxy New Test: Returns_null_when_server_returns_404 Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/PartDataRestServiceProxy.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/PartDataRestServiceProxy.java
@@ -1,8 +1,13 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.controller.PartDataController;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.PartData;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -12,13 +17,27 @@ import java.util.List;
 @Component
 public class PartDataRestServiceProxy {
 
+    private RestTemplate restTemplate;
+
     public PartDataRestServiceProxy(RestTemplateBuilder restTemplateBuilder){
-        restTemplateBuilder.build();
+            restTemplate = restTemplateBuilder.build();
     }
 
 
     public List<PartData> getPartDataList(String vinCode) {
 
-        throw new UnsupportedOperationException("Not Implemented");
+         try{
+
+             restTemplate.getForEntity(PartDataController.URL_MAPPING+"/"+vinCode, PartData[].class);
+         }catch(HttpClientErrorException e)
+         {
+             if(e.getStatusCode().equals(HttpStatus.NOT_FOUND))
+                 return null;
+             else
+                 throw e;
+         }
+
+         throw new UnsupportedOperationException("Not Implemented");
+
     }
 }

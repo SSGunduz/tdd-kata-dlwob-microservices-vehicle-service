6b6ab70ca779b8ba0c6ae66661b8f5846946def9
serdar.suat <serdar.suat@hexad.es>
2017-05-25 13:18:31 +0200

    CustomerDataRestServiceProxy New Test: Returns_null_when_server_returns_404 Implementation

--- src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/CustomerDataRestServiceProxy.java
+++ src/main/java/com/tdd/katas/dlwob/microservices/vehicleservice/service/CustomerDataRestServiceProxy.java
@@ -1,9 +1,14 @@
 package com.tdd.katas.dlwob.microservices.vehicleservice.service;
 
+import com.tdd.katas.dlwob.microservices.vehicleservice.controller.CustomerDataController;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.CustomerData;
 import com.tdd.katas.dlwob.microservices.vehicleservice.model.VehicleData;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Hexad GmbH on 25/05/2017.
@@ -11,14 +16,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomerDataRestServiceProxy {
 
+    private RestTemplate restTemplate;
 
     public CustomerDataRestServiceProxy(RestTemplateBuilder restTemplateBuilder){
-        restTemplateBuilder.build();
+        restTemplate=restTemplateBuilder.build();
     }
 
-    public CustomerData getCustomerData(String non_existent_vin_code) {
-        throw new UnsupportedOperationException("Not implemented");
+    public CustomerData getCustomerData(String customerId) {
+        try{
+            restTemplate.getForEntity(CustomerDataController.URL_MAPPING+"/"+customerId,CustomerData.class);
+        }catch(HttpClientErrorException e){
 
+            if(e.getStatusCode().equals(HttpStatus.NOT_FOUND))
+                return null;
+            else
+                throw e;
+        }
+
+        throw new UnsupportedOperationException("Not implemented");
     }
 
 }
